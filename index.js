a8d24124149e3ef43e2bef0cb3ce02f54d9a6aa3
Alpaca3245 <kyle.luu11@gmail.com>
2024-12-08 20:03:22 -0500

    Finished shuffle and combineNameAndSurname assignment

--- index.js
+++ index.js
@@ -27,5 +27,37 @@ console.log("Unsorted Array:    " + arrCopyAndSort);
 
 console.log("Map to names\n-------------------------\n")
 
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+// Mostly copied solution because I did not understand how to define a new object
+function combineNameAndSurname(user) {
+    return users.map(user => ({fullName: `${user.name} ${user.surname}`, id: user.id}));
+}
+
+let usersMapped = combineNameAndSurname(users)
+console.log(usersMapped);
+
+console.log("Map to names\n-------------------------\n")
+let shuffleArr = [1,2,3];
+
+function shuffle(arr) {
+    // () => passes an function with no argument and name and returns Math.random() -0.5, because typeError if there is no function
+    // sorts work by 1 
+    return arr.sort(() => Math.random() - 0.5);
+}
+
+console.log(shuffle(shuffleArr));
+console.log(shuffle(shuffleArr));
+console.log(shuffle(shuffleArr));
+
+console.log("Get average age\n-------------------------\n")
+
+
+
+
 
 
